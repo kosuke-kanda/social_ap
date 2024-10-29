a72d4c3d4bd3534b255cf840746fb50f6a4a9e74
kanda <mikomiko.lust.love@gmail.com>
2024-10-30 00:24:37 +0900

    URL遷移先変更、レイアウト調整

--- sns/urls.py
+++ sns/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "sns"
 
 urlpatterns = [
-    path('top/', views.TopView.as_view(), name="top"),
-    path("", views.TimeLineView.as_view(), name="list"),
+    path("", views.TopView.as_view(), name="top"),
+    path("list/", views.TimeLineView.as_view(), name="list"),
     path("user/<str:user>", views.UserPageView.as_view(), name="userPage"),
     path("create/", views.CreatePostView.as_view(), name="create"),
     path("delete/<int:pk>", views.DeltePost.as_view(), name="delete"),
